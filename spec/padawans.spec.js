a5c273696800ccd5d96013bd788019727e24b4b3
Taras <posh@live.ru>
2021-08-26 12:23:20 +0300

    sync read test case fixed

--- spec/padawans.spec.js
+++ spec/padawans.spec.js
@@ -8,7 +8,7 @@ const {
 
 describe('Статистика о падаванах', () => {
   it('getPadawanNames не использует синхронное чтение', () => {
-    expect(getPadawanNames.tostring()).not.toMatch(/Sync/);
+    expect(getPadawanNames.toString()).not.toMatch(/Sync/);
   });
   it('getPadawanNames возвращает список падаванов из файла `data/padawans.txt`', () => {
     const names = getPadawanNames();
