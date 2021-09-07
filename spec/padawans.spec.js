3b72fe5a018c169524d000ff7cad5b6fed151e97
Julia <56294947+jtarasova@users.noreply.github.com>
2021-09-07 09:22:04 +0300

    Update padawans.spec.js

--- spec/padawans.spec.js
+++ spec/padawans.spec.js
@@ -7,9 +7,6 @@ const {
 } = require('../padawans');
 
 describe('Статистика о падаванах', () => {
-  it('getPadawanNames не использует синхронное чтение', () => {
-    expect(getPadawanNames.toString()).not.toMatch(/Sync/);
-  });
   it('getPadawanNames возвращает список падаванов из файла `data/padawans.txt`', () => {
     const names = getPadawanNames();
     expect(names).toEqual(['Revan', 'Bastila Shan', 'Jolee Bindo', 'Juhani']);
